1decc03ab08ac6657f2380a9781d1c8687418c21
florianxr68 <florian.reckow@outlook.de>
2023-04-15 20:23:49 +0200

    Add test text analyzer

---
+++ src/main/java/de/fhdw/freefeser/LocationExtractor.java
@@ -0,0 +1,39 @@
+package de.fhdw.freefeser;
+
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.language.v1.*;
+import com.google.cloud.language.v1.Document.Type;
+import java.io.FileInputStream;
+import java.util.List;
+
+public class LocationExtractor {
+    public static String extractLocation(String text) throws Exception {
+        // Load the credentials from the config file
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/de/fhdw/freefeser/credentials.json"));
+
+        // Instantiates a client
+        try (LanguageServiceClient language = LanguageServiceClient.create(LanguageServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build())) {
+            // Set the text content and type (plain text)
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            // Detects entities in the document
+            AnalyzeEntitiesResponse response = language.analyzeEntities(doc);
+            List<Entity> entities = response.getEntitiesList();
+
+            // Look for entities that are locations and return the first one found
+            for (Entity entity : entities) {
+                if (entity.getType() == Entity.Type.LOCATION) {
+                    return entity.getName();
+                }
+            }
+
+            // If no location entity was found, return null
+            return null;
+        }
+    }
+}
--- src/main/java/de/fhdw/freefeser/Main.java
+++ src/main/java/de/fhdw/freefeser/Main.java
@@ -1,7 +1,9 @@
 package de.fhdw.freefeser;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String text = "Ich wohne in Berlin.";
+        String location = LocationExtractor.extractLocation(text);
+        System.out.println(location);
     }
 }

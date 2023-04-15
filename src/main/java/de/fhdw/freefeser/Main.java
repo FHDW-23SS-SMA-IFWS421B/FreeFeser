4f76191eb1817166ecd7d94e9b340ef43a1d7a7a
florianxr68 <florian.reckow@outlook.de>
2023-04-15 21:03:44 +0200

    Update Main.java

--- src/main/java/de/fhdw/freefeser/Main.java
+++ src/main/java/de/fhdw/freefeser/Main.java
@@ -3,7 +3,9 @@ package de.fhdw.freefeser;
 public class Main {
     public static void main(String[] args) throws Exception {
         String text = "Wie ist das Wetter in Berlin?";
+        String text2 = "Wie ist das Wetter in deiner Mutter?";
         String location = LocationExtractor.extractLocation(text);
-        System.out.println(location);
+        String location2 = LocationExtractor.extractLocation(text2);
+        System.out.println(location + "\n" + location2);
     }
 }

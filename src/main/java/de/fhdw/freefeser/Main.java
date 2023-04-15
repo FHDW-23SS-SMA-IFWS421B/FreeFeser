889ceeb0fc0d8a1e9e61c00733f8884fb44a1bd1
florianxr68 <florian.reckow@outlook.de>
2023-04-16 01:29:06 +0200

    Attempt to detect when the current and when the future weather should be displayed

--- src/main/java/de/fhdw/freefeser/Main.java
+++ src/main/java/de/fhdw/freefeser/Main.java
@@ -8,6 +8,8 @@ public class Main {
         String text2 = "Wie ist das Wetter in deiner Mutter?";
         String text3 = "wikiBot";
         String text4 = "WeatherBot";
+        String text5 = "Wie ist das Wetter in Berlin?";
+        String text6 = "Wie wird das Wetter in Bielefeld sein?";
 
         String location = TextAnalyzer.extractLocation(text);
         String location2 = TextAnalyzer.extractLocation(text2);
@@ -15,6 +17,10 @@ public class Main {
 
         String bot = TextAnalyzer.extractBot(text3);
         String bot2 = TextAnalyzer.extractBot(text4);
-        System.out.println(bot + "\n" + bot2);
+        System.out.println(bot + "\n" + bot2 + "\n");
+
+        String weather = TextAnalyzer.extractWeatherCurrentOrForecast(text5);
+        String weather2 = TextAnalyzer.extractWeatherCurrentOrForecast(text6);
+        System.out.println(weather + "\n" + weather2);
     }
 }
--- src/main/java/de/fhdw/freefeser/textanalyzer/TextAnalyzer.java
+++ src/main/java/de/fhdw/freefeser/textanalyzer/TextAnalyzer.java
@@ -8,35 +8,6 @@ import java.io.FileInputStream;
 import java.util.List;
 
 public class TextAnalyzer {
-    public static String extractLocation(String text) throws Exception {
-        // Load the credentials from the config file
-        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/de/fhdw/freefeser/textanalyzer/credentials.json"));
-
-        // Instantiates a client
-        try (LanguageServiceClient language = LanguageServiceClient.create(LanguageServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build())) {
-            // Set the text content and type (plain text)
-            Document doc = Document.newBuilder()
-                    .setContent(text)
-                    .setType(Type.PLAIN_TEXT)
-                    .build();
-
-            // Detects entities in the document
-            AnalyzeEntitiesResponse response = language.analyzeEntities(doc);
-            List<Entity> entities = response.getEntitiesList();
-
-            // Look for entities that are locations and return the first one found
-            for (Entity entity : entities) {
-                if (entity.getType() == Entity.Type.LOCATION) {
-                    // return in lowercase for further use
-                    return entity.getName().toLowerCase();
-                }
-            }
-
-            // If no location entity was found, return null
-            return null;
-        }
-    }
-
     public static String extractBot(String text) throws Exception {
         // Load the credentials from the config file
         GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/de/fhdw/freefeser/textanalyzer/credentials.json"));
@@ -67,6 +38,61 @@ public class TextAnalyzer {
 
             // If no bot entity was found, return null
             return null;
+        }
+    }
+
+    public static String extractLocation(String text) throws Exception {
+        // Load the credentials from the config file
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/de/fhdw/freefeser/textanalyzer/credentials.json"));
+
+        // Instantiates a client
+        try (LanguageServiceClient language = LanguageServiceClient.create(LanguageServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build())) {
+            // Set the text content and type (plain text)
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            // Detects entities in the document
+            AnalyzeEntitiesResponse response = language.analyzeEntities(doc);
+            List<Entity> entities = response.getEntitiesList();
+
+            // Look for entities that are locations and return the first one found
+            for (Entity entity : entities) {
+                if (entity.getType() == Entity.Type.LOCATION) {
+                    // return in lowercase for further use
+                    return entity.getName().toLowerCase();
+                }
+            }
+
+            // If no location entity was found, return null
+            return null;
+        }
+    }
+
+    public static String extractWeatherCurrentOrForecast(String text) throws Exception {
+        // Load the credentials from the config file
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/de/fhdw/freefeser/textanalyzer/credentials.json"));
+
+        // Instantiates a client
+        try (LanguageServiceClient language = LanguageServiceClient.create(LanguageServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build())) {
+            // Set the text content and type (plain text)
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            // Detects the sentiment of the text
+            Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+            // Extracts the weather type based on the detected sentiment
+            if (sentiment.getScore() > 0) {
+                // The user is asking for current weather
+                return "current";
+            } else {
+                // The user is asking for weather forecast
+                return "forecast";
+            }
         }
     }
 }

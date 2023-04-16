491a0b47f0d4281ec341a1d96681bfa2be76ca25
florianxr68 <florian.reckow@outlook.de>
2023-04-16 19:55:55 +0200

    Add DeeplAPI key for further use

---
+++ src/main/java/de/fhdw/freefeser/apis/DeeplAPI.java
@@ -0,0 +1,9 @@
+package de.fhdw.freefeser.apis;
+
+public class DeeplAPI {
+    private String apiKey = "3f881fa8-df8f-647c-9658-5cdf3e8f85ac:fx";
+
+    public DeeplAPI(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}

5e6350f8681b16b3e5901efc276c4e5576856e46
Florianxr68 <florian.reckow@outlook.de>
2023-04-20 12:57:43 +0200

    Delete old commit

--- src/main/java/de/fhdw/freefeser/chatbot/models/ChatSession.java
+++ src/main/java/de/fhdw/freefeser/chatbot/models/ChatSession.java
@@ -1,42 +1,4 @@
 package de.fhdw.freefeser.chatbot.models;
 
-import de.fhdw.freefeser.chatbot.interfaces.Bot;
-import de.fhdw.freefeser.chatbot.interfaces.Message;
-import de.fhdw.freefeser.chatbot.interfaces.User;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class ChatSession {
-    private final String sessionId;
-    private final User user;
-    private final Bot bot;
-    private final List<Message> messages;
-
-    public ChatSession(String sessionId, User user, Bot bot) {
-        this.sessionId = sessionId;
-        this.user = user;
-        this.bot = bot;
-        this.messages = new ArrayList<>();
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Bot getBot() {
-        return bot;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void addMessage(Message message) {
-        messages.add(message);
-    }
 }

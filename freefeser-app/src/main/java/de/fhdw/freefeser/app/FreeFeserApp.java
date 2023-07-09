778a0e6724d2d470c7a1ac331b922bf40b648ceb
florianxr68 <florian.reckow@outlook.de>
2023-07-09 20:10:48 +0200

    Establish h2 connection

--- freefeser-app/src/main/java/de/fhdw/freefeser/app/FreeFeserApp.java
+++ freefeser-app/src/main/java/de/fhdw/freefeser/app/FreeFeserApp.java
@@ -2,9 +2,17 @@ package de.fhdw.freefeser.app;
 
 import de.fhdw.freefeser.app.prototype.textanalyzer.TextAnalyzer;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import de.fhdw.freefeser.app.prototype.databases.AppUser;
+import de.fhdw.freefeser.app.util.HibernateUtil;
+
 public class FreeFeserApp {
     public static void main(String[] args) throws Exception {
-        String text = "Wie ist das Wetter in Berlin?";
+        /*String text = "Wie ist das Wetter in Berlin?";
         String text2 = "Wie ist das Wetter in deiner Mutter?";
         String text3 = "wikiBot";
         String text4 = "WeatherBot";
@@ -21,8 +29,32 @@ public class FreeFeserApp {
 
         String weather = TextAnalyzer.extractWeatherCurrentOrForecast(text5);
         String weather2 = TextAnalyzer.extractWeatherCurrentOrForecast(text6);
-        System.out.println(weather + "\n" + weather2);
+        System.out.println(weather + "\n" + weather2);*/
 
+        AppUser appUser = new AppUser();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(appUser);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
 
+        /*try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List <AppUser> appUsers = session.createQuery("from AppUser", AppUser.class).list();
+            appUsers.forEach(a -> System.out.println(a.getUserName()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }*/
     }
 }

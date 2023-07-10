351d30e74d47ad614d7197658ffe049a5acb2616
Fridious <philipp.friedhoff@outlook.de>
2023-07-10 12:46:16 +0200

    Improved chatbot

--- freefeser-api/src/main/java/de/fhdw/freefeser/api/bot/Chatbot.java
+++ freefeser-api/src/main/java/de/fhdw/freefeser/api/bot/Chatbot.java
@@ -3,9 +3,13 @@ package de.fhdw.freefeser.api.bot;
 
 import de.fhdw.freefeser.api.bot.command.CommandManager;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface Chatbot {
 
     String getName();
+
+    CompletableFuture<Boolean> isEnabled();
 
     CommandManager getCommandManager();
 }

977680a0ba9cc20ebb57061b91999a117341679e
florianxr68 <florian.reckow@outlook.de>
2023-07-18 20:08:40 +0200

    Add comments and AppUserManager, AppChatMessageManager

---
+++ freefeser-app/src/main/java/de/fhdw/freefeser/app/databases/managers/AppChatMessageManager.java
@@ -0,0 +1,34 @@
+package de.fhdw.freefeser.app.databases.managers;
+
+import de.fhdw.freefeser.api.database.ChatMessage;
+import de.fhdw.freefeser.api.database.ChatMessageManager;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class AppChatMessageManager implements ChatMessageManager {
+
+    @Override
+    public CompletableFuture<ChatMessage> load(UUID id) {
+        // SELECT Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Void> save(ChatMessage message) {
+        // UPDATE Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<ChatMessage> create(ChatMessage entityWithoutId) {
+        // INSERT Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Void> delete(UUID id) {
+        // DELETE Statement
+        return null;
+    }
+}
---
+++ freefeser-app/src/main/java/de/fhdw/freefeser/app/databases/managers/AppUserManager.java
@@ -0,0 +1,34 @@
+package de.fhdw.freefeser.app.databases.managers;
+
+import de.fhdw.freefeser.api.database.User;
+import de.fhdw.freefeser.api.database.UserManager;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class AppUserManager implements UserManager {
+
+    @Override
+    public CompletableFuture<User> load(UUID id) {
+        // SELECT Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Void> save(User user) {
+        // UPDATE Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<User> create(User entityWithoutId) {
+        // INSERT VALUE Statement
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Void> delete(UUID id) {
+        // DELETE Statement
+        return null;
+    }
+}

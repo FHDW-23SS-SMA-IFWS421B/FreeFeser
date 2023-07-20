8a906e1641c221fcdfb69a26d9fcb5b2b5fd3bcc
florianxr68 <florian.reckow@outlook.de>
2023-07-20 22:35:11 +0200

    Add info logger in main

--- freefeser-app/src/main/java/de/fhdw/freefeser/app/FreeFeserApp.java
+++ freefeser-app/src/main/java/de/fhdw/freefeser/app/FreeFeserApp.java
@@ -30,7 +30,7 @@ public class FreeFeserApp {
     public static void main(String[] args) throws Exception {
         Logger logger = LoggerFactory.getLogger(FreeFeserApp.class);
 
-        /*String text = "Wie ist das Wetter in Berlin?";
+        String text = "Wie ist das Wetter in Berlin?";
         String text2 = "Wie ist das Wetter in deiner Mutter?";
         String text3 = "wikiBot";
         String text4 = "WeatherBot";
@@ -39,17 +39,30 @@ public class FreeFeserApp {
 
         String location = TextAnalyzer.extractLocation(text);
         String location2 = TextAnalyzer.extractLocation(text2);
-        System.out.println(location + "\n" + location2 + "\n");
+        logger.info("Location 1: {}", location);
+        logger.info("");
+        logger.info("Location 2: {}", location2);
+        logger.info("");
+        logger.info("");
 
         String bot = TextAnalyzer.extractBot(text3);
         String bot2 = TextAnalyzer.extractBot(text4);
         System.out.println(bot + "\n" + bot2 + "\n");
+        logger.info("Bot 1: {}", bot);
+        logger.info("");
+        logger.info("Bot 2: {}", bot2);
+        logger.info("");
+        logger.info("");
 
         String weather = TextAnalyzer.extractWeatherCurrentOrForecast(text5);
         String weather2 = TextAnalyzer.extractWeatherCurrentOrForecast(text6);
-        System.out.println(weather + "\n" + weather2);
+        logger.info("Weather 1: {}", weather);
+        logger.info("");
+        logger.info("Weather 2: {}", weather2);
+        logger.info("");
+        logger.info("");
 
-        CommandManager commandManager = new AppCommandManager();
+        /*CommandManager commandManager = new AppCommandManager();
         commandManager.registerCommand(new TranslationCommand(null, "translate", "Translate something"));
 
         ConsoleReader reader = new AppConsoleReader(System.in);
@@ -60,15 +73,15 @@ public class FreeFeserApp {
 
         reader.addCallback(loginCallback);
 
-        reader.start();*/
+        reader.start();
 
         // Save some test data to the database
-        AppUserManager userManager = new AppUserManager();
+        /*AppUserManager userManager = new AppUserManager();
         AppChatMessageManager chatMessageManager = new AppChatMessageManager();
 
 
         // Save some test data to the database
-        /*AppUser user1 = new AppUser("user1", "password1");
+        AppUser user1 = new AppUser("user1", "password1");
         AppUser user2 = new AppUser("user2", "password2");
         userManager.create(user1);
         userManager.create(user2);
@@ -76,7 +89,7 @@ public class FreeFeserApp {
         AppChatMessage chatMessage1 = new AppChatMessage("Hello", LocalDateTime.now(), user1, null);
         AppChatMessage chatMessage2 = new AppChatMessage("Hi there", LocalDateTime.now(), user2, null);
         chatMessageManager.create(chatMessage1);
-        chatMessageManager.create(chatMessage2);*/
+        chatMessageManager.create(chatMessage2);
 
         // Test getAll() for AppUser
         CompletableFuture<List<User>> allUsersFuture = userManager.getAll();
@@ -121,6 +134,6 @@ public class FreeFeserApp {
             }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }

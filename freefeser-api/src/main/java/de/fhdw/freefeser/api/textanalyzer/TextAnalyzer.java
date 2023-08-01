e1bf61f155c9714926403c3d736afd59f62fff74
florianxr68 <florian.reckow@outlook.de>
2023-08-01 15:22:46 +0200

    Add finished TextAnalyzer interface

--- freefeser-api/src/main/java/de/fhdw/freefeser/api/textanalyzer/TextAnalyzer.java
+++ freefeser-api/src/main/java/de/fhdw/freefeser/api/textanalyzer/TextAnalyzer.java
@@ -3,6 +3,7 @@ package de.fhdw.freefeser.api.textanalyzer;
 import java.util.HashMap;
 
 public interface TextAnalyzer {
+
     HashMap<String, String> analyze(String text);
 
     String extractBot(String text);

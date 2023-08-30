b1d209efcb2135fdd4e23017e35042a1ada6fcfa
florianxr68 <florian.reckow@outlook.de>
2023-08-30 17:53:51 +0200

    Add commands !quit and !help

--- freefeser-app/src/main/java/de/fhdw/freefeser/app/chatbot/AppChatbotManager.java
+++ freefeser-app/src/main/java/de/fhdw/freefeser/app/chatbot/AppChatbotManager.java
@@ -2,9 +2,11 @@ package de.fhdw.freefeser.app.chatbot;
 
 import de.fhdw.freefeser.api.bot.Chatbot;
 import de.fhdw.freefeser.api.bot.ChatbotManager;
+import de.fhdw.freefeser.api.console.printer.ConsolePrinter;
 import de.fhdw.freefeser.api.database.ChatbotEntity;
 import de.fhdw.freefeser.api.database.ChatbotEntityDatabaseManager;
 import de.fhdw.freefeser.api.user.User;
+import de.fhdw.freefeser.app.console.printer.AppConsolePrinter;
 import de.fhdw.freefeser.app.databases.entities.AppChatbotEntity;
 import de.fhdw.freefeser.app.textanalyzer.AppTranslationTextAnalyzer;
 
@@ -16,10 +18,12 @@ public class AppChatbotManager implements ChatbotManager {
 
     private final Collection<Chatbot> bots;
     private final ChatbotEntityDatabaseManager databaseManager;
+    private final ConsolePrinter printer;
 
     public AppChatbotManager(ChatbotEntityDatabaseManager databaseManager) {
         this.databaseManager = databaseManager;
         this.bots = new ArrayList<>();
+        this.printer = new AppConsolePrinter();
     }
 
     @Override
@@ -45,6 +49,27 @@ public class AppChatbotManager implements ChatbotManager {
 
     @Override
     public Chatbot executeCommand(User sender, String text) {
+        if(text.equalsIgnoreCase("!help") || text.equalsIgnoreCase("help")) {
+            String helpMessage = "Chatbot-System Hilfe\n" +
+                    "====================\n" +
+                    "Verwendung der Bots:\n" +
+                    "@wikibot              Was/Wer ist <Name/Ort/etc.>?\n" +
+                    "@translationbot       Übersetze ins/auf <Ländercode (z.B. DE)>: <Text>\n" +
+                    "@weatherbot           Wie wird/ist das Wetter in <Ort> heute/morgen/nächste Woche/etc. ?\n\n" +
+                    "Befehle:\n" +
+                    "!help                 Zeigt diese Hilfe-Nachricht an\n" +
+                    "!activate <botname>   Aktiviert einen Bot\n" +
+                    "!deactivate <botname> Deaktiviert einen Bot\n" +
+                    "!list                 Listet alle Bots und deren Zustand auf\n" +
+                    "!quit                 Beendet das Programm\n";
+            this.printer.println(helpMessage);
+        }
+
+        if(text.equalsIgnoreCase("!quit") || text.equalsIgnoreCase("quit")) {
+            this.printer.println("Das Chatbot-System wird heruntergefahren.");
+            System.exit(0);
+        }
+
         String[] commandCheckTextRaw = text.split(" ", 2);
         if(commandCheckTextRaw.length != 2) {
             return null;
